6fb6b22803c929f9db89747ced62558a4d9bee05
xiao11lang <1023819715@qq.com>
2018-09-19 21:39:02 +0800

    增加文章概况组件

---
+++ src/components/article/Summary.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="summary">
+        <div class="title"></div>
+        <div class="content"></div>
+        <div class="extraInfo">
+            <span class="type"></span>
+            <span class="thumpUp"></span>
+            <span class="readCount"></span>
+            <span class="writeTime"></span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"Summary"
+}
+</script>
+<style lang="scss" scoped>
+    .summary{}
+</style>
