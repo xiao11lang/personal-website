9b4dc71e285d30ffab781fa0438e20e8ed86ec1f
xiao11lang <1023819715@qq.com>
2018-09-20 21:44:01 +0800

    完善文章概况组件

--- src/components/article/Article.vue
+++ src/components/article/Article.vue
@@ -1,15 +1,23 @@
 <template>
     <div class="article">
+        <Summary></Summary>
     </div>
     
 </template>
 <script>
+import Summary from './Summary'
 export default {
-    name:"Article"
+    name:"Article",
+    components:{
+        Summary
+    }
 }
 </script>
 <style lang="scss" scoped>
     .article{
-
+        margin: 30px 180px;
+        padding: 0 20px;
+        font-size: 14px;
+        background-color: white;
     }
 </style>
--- src/components/article/Summary.vue
+++ src/components/article/Summary.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="summary">
-        <div class="title"></div>
-        <div class="content"></div>
+        <div class="title">jstraning</div>
+        <div class="content">全栈工程师培训资料</div>
         <div class="extraInfo">
-            <span class="type"></span>
-            <span class="thumpUp"></span>
-            <span class="readCount"></span>
-            <span class="writeTime"></span>
+            <div class="typeCon">
+               <span class="iconfont icon-yuan"></span>
+               <span class="type">javascript</span> 
+            </div>
+            <div class="upCon">
+                <span class="iconfont icon-aixin"></span>
+                <span class="thumpUp">131</span>
+            </div>
+            <div class="countCon">
+                <span class="iconfont icon-kanguo"></span>
+                <span class="readCount">124</span>
+            </div>
+            <div class="timeCon">
+                <span class="writeTime">2018-12</span>
+            </div>
         </div>
     </div>
 </template>
@@ -16,5 +27,37 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    .summary{}
+    .summary{
+        width: 360px;
+        height: 130px;
+        border: 1px #e1e4e8 solid;
+        padding: 16px;
+        box-sizing: border-box;
+        border-radius: 3px;
+        .title{
+            color: #0366d6
+        }
+        .content{
+            color: #586069;
+            margin: 8px 0 16px 0;
+            font-size: 12px;
+            height: 36px
+        }
+        .extraInfo{
+            display: flex;
+            color: #586069;
+            div{
+                margin-right: 16px;
+                
+                span:before{
+                    font-size: 12px
+                }
+                &:nth-child(3){
+                    span::before{
+                        font-size: 16px
+                    }
+                }
+            }
+        }
+    }
 </style>
